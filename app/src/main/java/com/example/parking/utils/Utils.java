5d0a39524291934c7166e199877baa56d1d2fdfe
Zellerpooh <zellerpooh@gmail.com>
2017-05-12 08:58:31 +0800

    before mearge

--- app/src/main/java/com/example/parking/utils/Utils.java
+++ app/src/main/java/com/example/parking/utils/Utils.java
@@ -24,10 +24,10 @@ public class Utils {
     public static final String TEXT_POI_KEY = "停车场";
     public static final String TEXT_POI_TYPE = "交通设施服务";
     public static final int POI_SEARCH_BOUND = 10000;
-    public static final int POI_SEARCH_COUNT = 32;
+    public static final int POI_SEARCH_COUNT = 50;
     //讯飞语音
     public static final String XUN_FEI_APP_ID = "57d0dad1";
-    public static final int POI_SEARCH_COUNT = 50;
+
 
     public static void showSnackBar(View view, String str) {
         Snackbar.make(view, str, Snackbar.LENGTH_SHORT).setAction(R.string.know,
